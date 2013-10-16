f9b823e971f28472c562875c900f18d4e6ac5f48
str4d <str4d@mail.i2p>
2013-10-16 06:01:50 -0500

    Added common factory cancellation tests from txsocksx

--- txi2p/bob/test/test_factory.py
+++ txi2p/bob/test/test_factory.py
@@ -1,10 +1,16 @@
 # Copyright (c) str4d <str4d@mail.i2p>
 # See COPYING for details.
 
+from twisted.internet import defer
+from twisted.internet.error import ConnectionLost, ConnectionRefusedError
+from twisted.python import failure
 from twisted.test import proto_helpers
 from twisted.trial import unittest
 
 from txi2p.bob.factory import BOBI2PClientFactory, BOBI2PServerFactory
+
+connectionLostFailure = failure.Failure(ConnectionLost())
+connectionRefusedFailure = failure.Failure(ConnectionRefusedError())
 
 
 class BOBFactoryTestMixin(object):
@@ -19,8 +25,33 @@ class BOBFactoryTestMixin(object):
         proto.makeConnection(transport)
         return fac, proto
 
+    def test_cancellation(self):
+        fac, proto = self.makeProto(None, None, None, '')
+        fac.deferred.cancel()
+        self.assert_(self.aborted)
+        return self.assertFailure(fac.deferred, defer.CancelledError)
+
+    def test_cancellationBeforeFailure(self):
+        fac, proto = self.makeProto(None, None, None, '')
+        fac.deferred.cancel()
+        proto.connectionLost(connectionLostFailure)
+        self.assert_(self.aborted)
+        return self.assertFailure(fac.deferred, defer.CancelledError)
+
+    def test_cancellationAfterFailure(self):
+        fac, proto = self.makeProto(None, None, None, '')
+        proto.connectionLost(connectionLostFailure)
+        fac.deferred.cancel()
+        self.assertFalse(self.aborted)
+        return self.assertFailure(fac.deferred, ConnectionLost)
+
+    def test_clientConnectionFailed(self):
+        fac, proto = self.makeProto(None, None, None, '')
+        fac.clientConnectionFailed(None, connectionRefusedFailure)
+        return self.assertFailure(fac.deferred, ConnectionRefusedError)
+
     def test_defaultFactoryListsTunnels(self):
-        fac, proto = self.makeProto(object(), object(), object(), '')
+        fac, proto = self.makeProto(None, None, None, '')
         proto.dataReceived('BOB 00.00.10\nOK\n')
         self.assertEqual(proto.transport.value(), 'list\n')
 

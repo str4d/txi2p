9717d1fe96a33e5ce2a92683f789f0b40246cb14
str4d <str4d@mail.i2p>
2013-10-15 20:47:45 -0500

    Parse tunnel status in BOB grammar

--- txi2p/grammar.py
+++ txi2p/grammar.py
@@ -16,7 +16,23 @@ OK      = 'OK ' <(~'\n' anything)*>:info '\n' -> (True, info)
 OK_KEY  = 'OK ' KEY:pubkey '\n' -> (True, pubkey)
 OK_KEYS = 'OK ' KEYS:keys '\n' -> (True, keys)
 DATA    = 'DATA ' <(~'\n' anything)*>:data '\n' -> data
-OK_DATA = 'OK ' DATA:data -> data
+
+TUNNEL_STATUS = 'NICKNAME: ' <(~' ' anything)*>:nickname ' STARTING: ' <'true'|'false'>:starting ' RUNNING: ' <'true'|'false'>:running ' STOPPING: ' <'true'|'false'>:stopping ' KEYS: ' <'true'|'false'>:keys  ' QUIET: ' <'true'|'false'>:quiet ' INPORT: ' <(~' ' anything)*>:inport ' INHOST: ' <(~' ' anything)*>:inhost ' OUTPORT: ' <(~' ' anything)*>:outport ' OUTHOST: ' <(~'\n' anything)*>:outhost '\n' -> {
+    'nickname': nickname,
+    'starting': starting=='true',
+    'running': running=='true',
+    'stopping': stopping=='true',
+    'keys': keys=='true',
+    'quiet': quiet=='true',
+    'inport': inport,
+    'inhost': inhost,
+    'outport': outport,
+    'outhost': outhost
+    }
+
+DATA_TUNNEL_STATUS = 'DATA ' TUNNEL_STATUS:status -> status
+OK_TUNNEL_STATUS = 'OK ' TUNNEL_STATUS:status -> (True, status)
+OK_DATA_TUNNEL_STATUS = 'OK ' DATA_TUNNEL_STATUS:status -> (True, status)
 
 versionString = <digit+ '.' digit+ '.' digit+>
 BOB_init      = 'BOB ' versionString:version '\nOK\n' -> version
@@ -27,8 +43,8 @@ BOB_getkeys   = (ERROR | OK_KEYS)
 BOB_getnick   = (ERROR | OK)
 BOB_inhost    = (ERROR | OK)
 BOB_inport    = (ERROR | OK)
-BOB_list      = ((ERROR:(result, info)           -> (result, info, []))
-                |((DATA)*:data OK:(result, info) -> (result, info, data)))
+BOB_list      = ((ERROR:(result, info)                         -> (result, info, []))
+                |((DATA_TUNNEL_STATUS)*:data OK:(result, info) -> (result, info, data)))
 BOB_newkeys   = (ERROR | OK_KEY)
 BOB_option    = (ERROR | OK)
 BOB_outhost   = (ERROR | OK)
@@ -37,11 +53,12 @@ BOB_quiet     = (ERROR | OK)
 BOB_quit      = (OK)
 BOB_setkeys   = (ERROR | OK)
 BOB_setnick   = (ERROR | OK)
-BOB_show      = (ERROR | OK)
+BOB_show      = ((ERROR:(result, info)              -> (result, info, {}))
+                |(OK_TUNNEL_STATUS:(result, status) -> (result, '', status)))
 BOB_showprops = (ERROR | OK)
 BOB_start     = (ERROR | OK)
-BOB_status    = ((ERROR:(result, info)   -> (result, info, ()))
-                |(OK_DATA:(result, data) -> (result, '', data)))
+BOB_status    = ((ERROR:(result, info)                   -> (result, info, {}))
+                |(OK_DATA_TUNNEL_STATUS:(result, status) -> (result, '', status)))
 BOB_stop      = (ERROR | OK)
 BOB_verify    = (ERROR | OK)
 BOB_visit     = (OK)

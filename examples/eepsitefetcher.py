0e2431bec0f34bb1b4a967bc2825cf1fe5692fb6
str4d <str4d@mail.i2p>
2013-10-21 13:32:25 -0500

    Example Twisted client to fetch eepsite content

---
+++ examples/eepsitefetcher.py
@@ -0,0 +1,30 @@
+from sys import stdout
+from twisted.internet import reactor
+from twisted.internet.endpoints import clientFromString
+from twisted.internet.protocol import ClientFactory, Protocol
+
+from txi2p.bob.endpoints import BOBI2PClientEndpoint
+
+
+class Eepsite(Protocol):
+    def connectionMade(self):
+        print 'Connection made, sending eepsite request'
+        self.transport.write('GET / HTTP/1.1\n\n')
+
+    def dataReceived(self, data):
+        stdout.write(data)
+
+    def connectionLost(self, reason):
+        print 'Lost connection.  Reason:', reason
+        reactor.stop()
+
+
+class EepsiteFactory(ClientFactory):
+    protocol = Eepsite
+
+
+bobEndpoint = clientFromString(reactor, 'tcp:127.0.0.1:2828')
+endpoint = BOBI2PClientEndpoint(reactor, bobEndpoint, 'stats.i2p')
+d = endpoint.connect(EepsiteFactory())
+
+reactor.run()

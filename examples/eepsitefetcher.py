33edc5d994f6936274194fb1ad10b2143d7d8e02
str4d <str4d@mail.i2p>
2013-10-28 06:13:32 -0500

    Fixed eepsite fetcher example - connect to default BOB port

--- examples/eepsitefetcher.py
+++ examples/eepsitefetcher.py
@@ -23,7 +23,7 @@ class EepsiteFactory(ClientFactory):
     protocol = Eepsite
 
 
-bobEndpoint = clientFromString(reactor, 'tcp:127.0.0.1:2828')
+bobEndpoint = clientFromString(reactor, 'tcp:127.0.0.1:2827')
 endpoint = BOBI2PClientEndpoint(reactor, bobEndpoint, 'stats.i2p')
 d = endpoint.connect(EepsiteFactory())
 

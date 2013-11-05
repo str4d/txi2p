f9149ff8de2a8c5806f17011ef3ad00f9556fe36
str4d <str4d@mail.i2p>
2013-11-05 18:50:33 -0500

    More valid setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
 # Copyright (c) str4d <str4d@mail.i2p>
 # See COPYING for details.
 
-from distutils.core import setup
+from setuptools import setup
+
+
+with open('README.md', 'rb') as infile:
+    long_description = infile.read()
 
 with open('requirements.txt', 'rb') as infile:
     install_requires = infile.read().split()
 
 setup(
     name='txi2p',
-    description='I2P bindings for Twisted.',
+    version='0.1.0',
+    description='I2P bindings for Twisted',
+    long_description=long_description,
     author='str4d',
     author_email='str4d@mail.i2p',
     url='https://github.com/str4d/txi2p',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Internet',
+    ],
     license='ISC',
     install_requires=install_requires,
-    packages=['txi2p', 'txi2p.test'] + ['twisted.plugins'],
-    version='0.0.1',
+    packages=['txi2p', 'txi2p.bob', 'txi2p.test', 'txi2p.bob.test'] #+ ['twisted.plugins'],
 )
 
 # Make Twisted regenerate the dropin.cache, if possible.  This is necessary

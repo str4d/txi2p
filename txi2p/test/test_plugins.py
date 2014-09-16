0f674b6b6b45381b3043abb078725185448da1da
str4d <str4d@mail.i2p>
2014-09-16 05:30:12 +0000

    Import correct class

--- txi2p/test/test_plugins.py
+++ txi2p/test/test_plugins.py
@@ -7,7 +7,7 @@ from twisted.trial import unittest
 
 from txi2p.bob.endpoints import (BOBI2PClientEndpoint,
                                  BOBI2PServerEndpoint)
-from txi2p.plugins import _I2PClientParser
+from txi2p.plugins import I2PClientParser
 
 
 class I2PClientEndpointPluginTest(unittest.TestCase):
@@ -15,7 +15,7 @@ class I2PClientEndpointPluginTest(unittest.TestCase):
     Unit tests for the I2P client endpoint description parser.
     """
 
-    _parserClass = _I2PClientParser
+    _parserClass = I2PClientParser
 
     def test_pluginDiscovery(self):
         parsers = list(getPlugins(

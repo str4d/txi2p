c4a1cc2075356b56570b0cda979ccd580bec7246
str4d <str4d@mail.i2p>
2015-09-05 02:30:55 +0000

    Separate fields for host and Destination in I2PAddress

--- txi2p/address.py
+++ txi2p/address.py
@@ -11,25 +11,31 @@ class I2PAddress(FancyEqMixin, object):
     """
     An L{I2PAddress} represents the address of an L{I2PEndpoint}.
 
-    @ivar destination: An I2P Destination byte string; for example, b'example.i2p'.
+    @ivar destination: An I2P Destination byte string.
     @type destination: L{bytes}
+
+    @ivar host: An I2P host byte string; for example, b'example.i2p' or
+    b'fiftytwocharacters.b32.i2p'. If looked up, it is guaranteed to resolve to
+    destination.
+    @type host: L{bytes}
 
     @ivar port: (Optional) An integer representing the port number.
     @type port: L{int}
     """
     compareAttributes = ('destination', 'port')
 
-    def __init__(self, destination, port=None):
+    def __init__(self, destination, host=None, port=None):
         self.destination = destination
+        self.host = host
         self.port = int(port) if port else None
 
 
     def __repr__(self):
         if self.port:
             return '%s(%s, %d)' % (
-                self.__class__.__name__, self.destination, self.port)
+                self.__class__.__name__, self.host if self.host else self.destination, self.port)
         return '%s(%s)' % (
-            self.__class__.__name__, self.destination)
+            self.__class__.__name__, self.host if self.host else self.destination)
 
 
     def __hash__(self):
--- txi2p/test/test_address.py
+++ txi2p/test/test_address.py
@@ -7,38 +7,62 @@ from txi2p.address import I2PAddress
 
 
 class TestI2PAddress(unittest.TestCase):
-    def test_noPort(self):
-        addr = I2PAddress('spam.i2p')
-        self.assertEqual((addr.destination, addr.port), ('spam.i2p', None))
+    def test_noHost_noPort(self):
+        addr = I2PAddress('DESTINATION')
+        self.assertEqual((addr.destination, addr.host, addr.port), ('DESTINATION', None, None))
 
-    def test_port(self):
-        addr = I2PAddress('spam.i2p', 81)
-        self.assertEqual((addr.destination, addr.port), ('spam.i2p', 81))
+    def test_host_noPort(self):
+        addr = I2PAddress('DESTINATION', 'spam.i2p')
+        self.assertEqual((addr.destination, addr.host, addr.port), ('DESTINATION', 'spam.i2p', None))
+
+    def test_noHost_port(self):
+        addr = I2PAddress('DESTINATION', port=81)
+        self.assertEqual((addr.destination, addr.host, addr.port), ('DESTINATION', None, 81))
+
+    def test_host_port(self):
+        addr = I2PAddress('DESTINATION', 'spam.i2p', 81)
+        self.assertEqual((addr.destination, addr.host, addr.port), ('DESTINATION', 'spam.i2p', 81))
 
     def test_portString(self):
-        addr = I2PAddress('spam.i2p', '81')
-        self.assertEqual((addr.destination, addr.port), ('spam.i2p', 81))
+        addr = I2PAddress('DESTINATION', 'spam.i2p', '81')
+        self.assertEqual((addr.destination, addr.host, addr.port), ('DESTINATION', 'spam.i2p', 81))
 
-    def test_reprWithNoPort(self):
-        addr = I2PAddress('spam.i2p')
+    def test_reprWithNoHostNoPort(self):
+        addr = I2PAddress('DESTINATION')
+        self.assertEqual(repr(addr), 'I2PAddress(DESTINATION)')
+
+    def test_reprWithHostNoPort(self):
+        addr = I2PAddress('DESTINATION', 'spam.i2p')
         self.assertEqual(repr(addr), 'I2PAddress(spam.i2p)')
 
-    def test_reprWithPort(self):
-        addr = I2PAddress('spam.i2p', 81)
+    def test_reprWithNoHostPort(self):
+        addr = I2PAddress('DESTINATION', port=81)
+        self.assertEqual(repr(addr), 'I2PAddress(DESTINATION, 81)')
+
+    def test_reprWithHostPort(self):
+        addr = I2PAddress('DESTINATION', 'spam.i2p', 81)
         self.assertEqual(repr(addr), 'I2PAddress(spam.i2p, 81)')
 
     def test_reprWithPortString(self):
-        addr = I2PAddress('spam.i2p', '81')
+        addr = I2PAddress('DESTINATION', 'spam.i2p', '81')
         self.assertEqual(repr(addr), 'I2PAddress(spam.i2p, 81)')
 
-    def test_hashWithNoPort(self):
-        addr = I2PAddress('spam.i2p')
-        self.assertEqual(hash(addr), hash(('spam.i2p',None)))
+    def test_hashWithNoHostNoPort(self):
+        addr = I2PAddress('DESTINATION')
+        self.assertEqual(hash(addr), hash(('DESTINATION', None)))
 
-    def test_hashWithPort(self):
-        addr = I2PAddress('spam.i2p', 81)
-        self.assertEqual(hash(addr), hash(('spam.i2p', 81)))
+    def test_hashWithHostNoPort(self):
+        addr = I2PAddress('DESTINATION', 'spam.i2p')
+        self.assertEqual(hash(addr), hash(('DESTINATION', None)))
+
+    def test_hashWithNoHostPort(self):
+        addr = I2PAddress('DESTINATION', port=81)
+        self.assertEqual(hash(addr), hash(('DESTINATION', 81)))
+
+    def test_hashWithHostPort(self):
+        addr = I2PAddress('DESTINATION', 'spam.i2p', 81)
+        self.assertEqual(hash(addr), hash(('DESTINATION', 81)))
 
     def test_hashWithPortString(self):
-        addr = I2PAddress('spam.i2p', '81')
-        self.assertEqual(hash(addr), hash(('spam.i2p', 81)))
+        addr = I2PAddress('DESTINATION', 'spam.i2p', '81')
+        self.assertEqual(hash(addr), hash(('DESTINATION', 81)))

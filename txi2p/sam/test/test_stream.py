b979bb912b7ba5e42774e8db5173804c3681d9b9
str4d <str4d@mail.i2p>
2015-09-10 10:17:26 +0000

    STREAM FORWARD tests

--- txi2p/sam/test/test_stream.py
+++ txi2p/sam/test/test_stream.py
@@ -88,7 +88,7 @@ class TestStreamConnectProtocol(SAMProtocolTestMixin, unittest.TestCase):
         proto.dataReceived('HELLO REPLY RESULT=OK VERSION=3.1\n')
         proto.transport.clear()
         proto.dataReceived('STREAM STATUS RESULT=OK\n')
-        fac.streamConnectionEstablished.assert_called()
+        fac.streamConnectionEstablished.assert_called_with(proto.receiver)
 
 
 class TestStreamConnectFactory(SAMFactoryTestMixin, unittest.TestCase):
@@ -108,3 +108,59 @@ class TestStreamConnectFactory(SAMFactoryTestMixin, unittest.TestCase):
         streamProto = self.successResultOf(fac.deferred)
         self.assertEqual(proto.receiver.wrappedProto, streamProto)
     test_streamConnectionEstablished.skip = skipSRO
+
+
+class TestStreamForwardProtocol(SAMProtocolTestMixin, unittest.TestCase):
+    protocol = stream.StreamForwardProtocol
+
+    def test_streamForwardAfterHello(self):
+        fac, proto = self.makeProto()
+        fac.session = Mock()
+        fac.session.id = 'foo'
+        fac.forwardPort = 1337
+        proto.transport.clear()
+        proto.dataReceived('HELLO REPLY RESULT=OK VERSION=3.1\n')
+        self.assertEquals(
+            'STREAM FORWARD ID=foo PORT=1337 SILENT=false\n',
+            proto.transport.value())
+
+    def test_streamForwardReturnsError(self):
+        fac, proto = self.makeProto()
+        fac.session = Mock()
+        fac.session.id = 'foo'
+        fac.forwardPort = 1337
+        proto.transport.clear()
+        proto.dataReceived('HELLO REPLY RESULT=OK VERSION=3.1\n')
+        proto.transport.clear()
+        proto.dataReceived('STREAM STATUS RESULT=I2P_ERROR MESSAGE="foo bar baz"\n')
+        fac.resultNotOK.assert_called_with('I2P_ERROR', 'foo bar baz')
+
+    def test_streamForwardEstablishedAfterStreamForward(self):
+        fac, proto = self.makeProto()
+        fac.streamForwardEstablished = Mock()
+        fac.session = Mock()
+        fac.session.id = 'foo'
+        fac.forwardPort = 1337
+        proto.transport.clear()
+        proto.dataReceived('HELLO REPLY RESULT=OK VERSION=3.1\n')
+        proto.transport.clear()
+        proto.dataReceived('STREAM STATUS RESULT=OK\n')
+        fac.streamForwardEstablished.assert_called_with(proto.receiver)
+
+
+class TestStreamForwardFactory(SAMFactoryTestMixin, unittest.TestCase):
+    factory = stream.StreamForwardFactory
+    blankFactoryArgs = (Mock(), '')
+
+    def test_streamForwardEstablished(self):
+        mreactor = proto_helpers.MemoryReactor()
+        session = Mock()
+        fac, proto = self.makeProto(session, '')
+        # Shortcut to end of SAM stream forward protocol
+        proto.receiver.currentRule = 'State_forward'
+        proto._parser._setupInterp()
+        proto.dataReceived('STREAM STATUS RESULT=OK\n')
+        session.addStream.assert_called_with(proto.receiver)
+        streamProto = self.successResultOf(fac.deferred)
+        self.assertEqual(proto.receiver, streamProto)
+    test_streamForwardEstablished.skip = skipSRO

a0c53a638a5426a15d2f583006a9ed470614942d
str4d <str4d@mail.i2p>
2015-09-10 13:27:20 +0000

    Test SAM API instantiation via Endpoint strings

--- txi2p/test/test_plugins.py
+++ txi2p/test/test_plugins.py
@@ -10,6 +10,8 @@ from zope.interface.verify import verifyObject
 
 from txi2p.bob.endpoints import (BOBI2PClientEndpoint,
                                  BOBI2PServerEndpoint)
+from txi2p.sam.endpoints import (SAMI2PStreamClientEndpoint,
+                                 SAMI2PStreamServerEndpoint)
 
 if twisted.version < Version('twisted', 14, 0, 0):
     skip = 'txi2p.plugins requires twisted 14.0 or newer'
@@ -47,16 +49,6 @@ class I2PClientEndpointPluginTest(I2PPluginTestMixin, unittest.TestCase):
         from txi2p.plugins import I2PClientParser
         return I2PClientParser
 
-    def test_stringDescription(self):
-        from twisted.internet.endpoints import clientFromString
-        ep = clientFromString(
-            MemoryReactor(), "i2p:stats.i2p:api=BOB:tunnelNick=spam:inport=12345")
-        self.assertIsInstance(ep, BOBI2PClientEndpoint)
-        self.assertIsInstance(ep._reactor, MemoryReactor)
-        self.assertEqual(ep._dest,"stats.i2p")
-        self.assertEqual(ep._tunnelNick,"spam")
-        self.assertEqual(ep._inport,12345)
-
     def test_badAPI(self):
         from twisted.internet.endpoints import clientFromString
         self.failUnlessRaises(ValueError, clientFromString,
@@ -66,6 +58,30 @@ class I2PClientEndpointPluginTest(I2PPluginTestMixin, unittest.TestCase):
         from twisted.internet.endpoints import clientFromString
         self.failUnlessRaises(ValueError, clientFromString,
             MemoryReactor(), "i2p:stats.i2p:apiEndpoint=tcp\:127.0.0.1\:2827")
+
+    def test_stringDescription_default(self):
+        from twisted.internet.endpoints import clientFromString
+        ep = clientFromString(
+            MemoryReactor(), "i2p:stats.i2p")
+        self.assertIsInstance(ep, BOBI2PClientEndpoint)
+
+    def test_stringDescription_BOB(self):
+        from twisted.internet.endpoints import clientFromString
+        ep = clientFromString(
+            MemoryReactor(), "i2p:stats.i2p:api=BOB:tunnelNick=spam:inport=12345")
+        self.assertIsInstance(ep, BOBI2PClientEndpoint)
+        self.assertIsInstance(ep._reactor, MemoryReactor)
+        self.assertEqual(ep._dest,"stats.i2p")
+        self.assertEqual(ep._tunnelNick,"spam")
+        self.assertEqual(ep._inport,12345)
+
+    def test_stringDescription_SAM(self):
+        from twisted.internet.endpoints import clientFromString
+        ep = clientFromString(
+            MemoryReactor(), "i2p:stats.i2p:81:api=SAM")
+        self.assertIsInstance(ep, SAMI2PStreamClientEndpoint)
+        self.assertEqual(ep._host, "stats.i2p")
+        self.assertEqual(ep._port, 81)
 
 
 class I2PServerEndpointPluginTest(I2PPluginTestMixin, unittest.TestCase):
@@ -82,16 +98,6 @@ class I2PServerEndpointPluginTest(I2PPluginTestMixin, unittest.TestCase):
         from txi2p.plugins import I2PServerParser
         return I2PServerParser
 
-    def test_stringDescription(self):
-        from twisted.internet.endpoints import serverFromString
-        ep = serverFromString(
-            MemoryReactor(), "i2p:/tmp/testkeys.foo:api=BOB:tunnelNick=spam:outport=23456")
-        self.assertIsInstance(ep, BOBI2PServerEndpoint)
-        self.assertIsInstance(ep._reactor, MemoryReactor)
-        self.assertEqual(ep._keypairPath, "/tmp/testkeys.foo")
-        self.assertEqual(ep._tunnelNick, "spam")
-        self.assertEqual(ep._outport, 23456)
-
     def test_badAPI(self):
         from twisted.internet.endpoints import serverFromString
         self.failUnlessRaises(ValueError, serverFromString,
@@ -101,3 +107,27 @@ class I2PServerEndpointPluginTest(I2PPluginTestMixin, unittest.TestCase):
         from twisted.internet.endpoints import serverFromString
         self.failUnlessRaises(ValueError, serverFromString,
             MemoryReactor(), "i2p:/tmp/testkeys.foo:apiEndpoint=tcp\:127.0.0.1\:2827")
+
+    def test_stringDescription_default(self):
+        from twisted.internet.endpoints import serverFromString
+        ep = serverFromString(
+            MemoryReactor(), "i2p:/tmp/testkeys.foo")
+        self.assertIsInstance(ep, BOBI2PServerEndpoint)
+
+    def test_stringDescription_BOB(self):
+        from twisted.internet.endpoints import serverFromString
+        ep = serverFromString(
+            MemoryReactor(), "i2p:/tmp/testkeys.foo:api=BOB:tunnelNick=spam:outport=23456")
+        self.assertIsInstance(ep, BOBI2PServerEndpoint)
+        self.assertIsInstance(ep._reactor, MemoryReactor)
+        self.assertEqual(ep._keypairPath, "/tmp/testkeys.foo")
+        self.assertEqual(ep._tunnelNick, "spam")
+        self.assertEqual(ep._outport, 23456)
+
+    def test_stringDescription_SAM(self):
+        from twisted.internet.endpoints import serverFromString
+        ep = serverFromString(
+            MemoryReactor(), "i2p:/tmp/testkeys.foo:81:api=SAM")
+        self.assertIsInstance(ep, SAMI2PStreamServerEndpoint)
+        self.assertIsInstance(ep._reactor, MemoryReactor)
+        self.assertEqual(ep._port, 81)

4bdcc9048e33b0965307d90597a45984c48e4646
str4d <str4d@mail.i2p>
2016-06-19 13:14:53 +1200

    Continue loading txi2p if txi2p._version doesn't exist

--- txi2p/__init__.py
+++ txi2p/__init__.py
@@ -1,4 +1,7 @@
-from txi2p._version import __version__, __revision__
+try:
+    from txi2p._version import __version__, __revision__
+except ImportError:
+    __version__ = __revision__ = None
 
 from txi2p.address import I2PAddress
 from txi2p.utils import generateDestination
